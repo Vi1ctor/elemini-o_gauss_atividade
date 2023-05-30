6a961b6b8aa156848fd10739fa554ed0f1955ae3
Vi1ctor <82913507+Vi1ctor@users.noreply.github.com>
2023-05-29 22:02:19 -0300

    Elinição de Gauss e Gauss-Seidel python 3.9.0
    Certifique-se de usar a versão correto do python
    código utilizando biblioteca numpy para o cálculo de matrizes

--- main.py
+++ main.py
@@ -1,44 +1,58 @@
-# -*- coding: utf-8 -*-     #codificação do arquivo padronizada para UTF-8
-import math    #importa o modulo math do python que nos possibilita usar funçções matemáticas úteis
+import numpy as np
 
-def eliminacao_gauss(A, b):  #função que recebe como parametro uma matriz A, e um vetor relacionado ao termo independente
-    n = len(A)  #determinar o tamanho do sistema linear  n= o número de equações/variáveis do sistema, no caso usado (3).
+def eliminacao_gauss(A, b):
+    n = len(A)
+    for k in range(n-1):
+        for i in range(k+1, n):
+            fator = A[i, k] / A[k, k]
+            for j in range(k, n):
+                A[i, j] = A[i, j] - fator * A[k, j]
+            b[i] = b[i] - fator * b[k]
+    return A, b
 
-    for i in range(n):
-        if A[i][i] == 0:
-            raise ValueError('Divisão por zero encontrada!') #for para percorrer a diagnal principal(se achar um zero ocasionaria em uma indeterminação)
-                                                             # Neste caso uma divisão por zero
-        for j in range(i + 1, n):
-            fator = A[j][i] / A[i][i]    #percorrem as linhas abaixo da diagonal principal e calculam o fator de eliminação
-
-
-            for k in range(i, n):
-                A[j][k] -= fator * A[i][k]
-
-            b[j] -= fator * b[i]
-
-        print("Matriz A após a etapa", i + 1)
-        for row in A:
-            print(row)
-        print()
-
-    x = [0] * n    #lista preenchida de zeros para guardar os resultados
-
-    for i in range(n - 1, -1, -1):
-        soma = sum(A[i][j] * x[j] for j in range(i + 1, n))
-        x[i] = (b[i] - soma) / A[i][i]
-
+def gauss_seidel(A, b, N, epsilon):
+    n = len(A)
+    x = np.zeros(n)
+    for _ in range(N):
+        x_prev = np.copy(x)
+        for i in range(n):
+            sum1 = np.dot(A[i, :i], x[:i])
+            sum2 = np.dot(A[i, i+1:], x_prev[i+1:])
+            x[i] = (b[i] - sum1 - sum2) / A[i, i]
+        if np.linalg.norm(x - x_prev) < epsilon:
+            break
     return x
 
+# Exemplo de uso
+n = int(input("Digite o tamanho da matriz (n): "))
+A = np.zeros((n, n))
+print("Digite os elementos da matriz A:")
+for i in range(n):
+    for j in range(n):
+        A[i, j] = float(input(f"A[{i+1}, {j+1}]: "))
 
-A = [[2, 1, -1],
-     [-3, -1, 2],
-     [-2, 1, 2]]
+b = np.zeros(n)
+print("Digite os elementos do vetor b:")
+for i in range(n):
+    b[i] = float(input(f"b[{i+1}]: "))
 
-b = [8, -11, -3]
+metodo = input("Escolha o método (Gauss-Seidel ou eliminação de gauss): ")
 
-solucao = eliminacao_gauss(A, b)
+if metodo.lower() == "gauss-seidel":
+    N = int(input("Digite o número máximo de iterações (N): "))
+    epsilon = float(input("Digite o valor de Épsilon: "))
 
-print("Solução:")
-for i, x in enumerate(solucao):
-    print("x{} = {}".format(i+1, x))
+    x = gauss_seidel(A, b, N, epsilon)
+    print("Solução aproximada:")
+    print(x)
+elif metodo.lower() == "eliminacao de gauss" or metodo.lower() == "eliminação de gauss":
+    A_final, b_final = eliminacao_gauss(A, b)
+
+    print("Após a eliminação de Gauss:")
+    print("Matriz A:")
+    print(A_final)
+    print("Vetor b:")
+    print(b_final)
+else:
+    print("Método não reconhecido. Por favor, escolha Gauss-Seidel ou Eliminação de Gauss.")
+
